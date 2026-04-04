610f68b489d5b3219ee5e81443b9b7594159290d
Alchyr <isithalchyr@gmail.com>
2026-04-04 00:38:13 -0700

    add compatibiltiy with both beta and main branch

--- Abstracts/CustomSingletonModel.cs
+++ Abstracts/CustomSingletonModel.cs
@@ -1,5 +1,8 @@
-﻿using MegaCrit.Sts2.Core.Modding;
+﻿using System.Reflection;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
 
 namespace BaseLib.Abstracts;
 
@@ -8,6 +11,14 @@ namespace BaseLib.Abstracts;
 /// </summary>
 public abstract class CustomSingletonModel : SingletonModel, ICustomModel
 {
+    private static readonly MethodInfo? SubscribeCombat = typeof(ModHelper).GetMethod("SubscribeForCombatStateHooks");
+    private static readonly MethodInfo? SubscribeRunState = typeof(ModHelper).GetMethod("SubscribeForRunStateHooks");
+
+    private static readonly Type? RunHookSubDelegate =
+        Type.GetType("MegaCrit.Sts2.Core.Modding.RunHookSubscriptionDelegate, sts2");
+    private static readonly Type? CombatHookSubDelegate =
+        Type.GetType("MegaCrit.Sts2.Core.Modding.CombatHookSubscriptionDelegate, sts2");
+    
     /// <summary>
     /// This property seems effectively unused; it is set anyways in case of future changes.
     /// </summary>
@@ -16,9 +27,29 @@ public abstract class CustomSingletonModel : SingletonModel, ICustomModel
     public CustomSingletonModel(bool receiveCombatHooks, bool receiveRunHooks)
     {
         ShouldReceiveCombatHooks = receiveCombatHooks;
-        if (receiveCombatHooks)
-            ModHelper.SubscribeForCombatStateHooks(Id.Entry, _ => [this]);
+
+        if (SubscribeCombat == null || SubscribeRunState == null)
+        {
+            BaseLibMain.Logger.Warn($"CustomSingleton {GetType().FullName} created; not supported on current game branch");
+            return;
+        }
+
         if (receiveRunHooks)
-            ModHelper.SubscribeForRunStateHooks(Id.Entry, _ => [this]);
+        {
+            SubscribeRunState.Invoke(null, [Id.Entry, Delegate.CreateDelegate(RunHookSubDelegate!, this, typeof(CustomSingletonModel).GetMethod("RunSubModels")!)]);
+        }
+        if (receiveCombatHooks)
+        {
+            SubscribeCombat.Invoke(null, [Id.Entry, Delegate.CreateDelegate(CombatHookSubDelegate!, this, typeof(CustomSingletonModel).GetMethod("CombatSubModels")!)]);
+        }
+    }
+
+    public IEnumerable<AbstractModel> RunSubModels(RunState runState)
+    {
+        return [this];
+    }
+    public IEnumerable<AbstractModel> CombatSubModels(CombatState combatState)
+    {
+        return [this];
     }
 }
